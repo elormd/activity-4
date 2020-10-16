78aee4ffecc821834bd43fb3e3da8a3e05c76bb3
elormd <dziks20052@gmail.com>
2020-10-16 13:47:04 +0000

    this is activity 4

---
+++ first.py
@@ -0,0 +1,23 @@
+your_day = 'How are you doing today?'
+print(your_day)
+your_day = input()
+COVID_EFFECT_ANALYSIS =  "how are you battling with the reign of COVID 19."
+print(COVID_EFFECT_ANALYSIS)
+COVID_EFFECT_ANALYSIS = input()
+your_mood = 'On a scale of 1-10 please rate how bored you are'
+print('On a scale of 1-10 please rate how bored you are')
+your_mood = input()
+if your_mood == range(5-10):
+    print('You are really bored, find a hobby.')
+else:
+    print('You are really enjoying your day.')
+your_attitude = 'how bad is your attitude'
+
+print('On a scale of 1-100 please rate how bad your attitude is towards your friends')
+your_attitude = input()
+if your_attitude == 1-50:
+    print('you really need to seek help immediately.')
+elif  your_attitude == range(51-80):
+    print("your attitude is managable")
+else:
+    print("your attitude is just perfect")
